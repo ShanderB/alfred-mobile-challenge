90b84c1fa984c5fdf47a866a8fd99750eca255c4
Shander Andrade <s.hander8910@hotmail.com>
2022-09-06 08:41:11 -0300

    {SHANDER}[Home] adicionado tabs de navegacao

--- desafio-startup/App.tsx
+++ desafio-startup/App.tsx
@@ -3,15 +3,24 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from "./src/components/HomeScreen/HomeScreen"
+import FavoriteScreen from "./src/components/FavoriteScreen/FavoriteScreen"
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return(
     <NavigationContainer>
-      <Stack.Navigator>
+      {/* <Stack.Navigator>
         <Stack.Screen name="Pacientes" component={HomeScreen}/>
-      </Stack.Navigator>
+      </Stack.Navigator> */}
+      <Tab.Navigator initialRouteName={"HomeScreen"} screenOptions={({route}) => ({
+      
+      })}>
+        <Tab.Screen name={"HomeScreen"} component={HomeScreen}></Tab.Screen>
+        <Tab.Screen name={"Favorite"} component={FavoriteScreen}></Tab.Screen>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
